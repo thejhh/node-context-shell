1056a8866f2cdb63476b5da1ac52b1fd48d6fa74
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-07-30 18:41:55 +0300

    Updated

--- protos/example.js
+++ protos/example.js
@@ -1,7 +1,9 @@
 
 /* The Library */
 
-var shell = require('./simple-shell.js');
+var shell = require('./simple-shell.js'),
+    foreach = require('snippets').foreach,
+    sys = require('sys');
 
 /* The Example */
 
@@ -10,12 +12,36 @@ function exit() {
 	process.exit();
 }
 
+function help() {
+	var commands = [], props = {}, isprops=false;
+	foreach(this).do(function(value, key) {
+		if(value && (typeof value === 'function')) {
+			props[key] = value;
+			isprops = true;
+		} else {
+			commands.push(key);
+		}
+	});
+	
+	if(commands.length !== 0) {
+		console.log("List of commands:");
+		foreach(commands).do(function(key) {
+			console.log("  "+key);
+		});
+	}
+	
+	if(isprops) {
+		console.log("List of properties:");
+		foreach(commands).do(function(value, key) {
+			console.log("  "+key+ " = " + sys.inspect(value));
+		});
+	}
+}
+
 var context = {
+	'help': help,
 	'echo': function(a, b, c) {
 		console.log(a + ", " + b + ", " + c);
-	},
-	'help': function() {
-		console.log('User wrote help');
 	},
 	'exit': exit,
 	'quit': exit
