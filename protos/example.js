c64796f545f4b9c5997a745be9c1d947cdc5ca23
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-07-31 00:18:27 +0300

    Updated

--- protos/example.js
+++ protos/example.js
@@ -2,6 +2,7 @@
 /* The Library */
 
 var shell = require('./simple-shell.js'),
+	merge = require('snippets').merge,
     foreach = require('snippets').foreach,
     sys = require('sys');
 
@@ -12,16 +13,28 @@ function exit() {
 	process.exit();
 }
 
-function help() {
-	var commands = [], props = {}, isprops=false;
-	foreach(this).do(function(value, key) {
+function ls() {
+	var prompt = this;
+	
+	var commands = [], props = {}, isprops=false, contexts=[];
+	foreach(prompt.context, prompt.buildin_context).do(function(value, key) {
+		if(!key) return;
 		if(value && (typeof value === 'function')) {
+			commands.push(key);
+		} else if(value && (typeof value === 'object')) {
+			contexts.push(key);
+		} else {
 			props[key] = value;
 			isprops = true;
-		} else {
-			commands.push(key);
 		}
 	});
+	
+	if(contexts.length !== 0) {
+		console.log("List of contexts:");
+		foreach(contexts).do(function(key) {
+			console.log("  "+key);
+		});
+	}
 	
 	if(commands.length !== 0) {
 		console.log("List of commands:");
@@ -38,16 +51,59 @@ function help() {
 	}
 }
 
+function set_prop(key, value) {
+	var prompt = this,
+	    context = prompt.context;
+	if(!context.hasOwnProperty(key)) prompt.emit('error', 'No such keyword: ' + key);
+	else context[key] = value;
+}
+
+function cd(key) {
+	var prompt = this,
+	    context = prompt.context;
+	
+	if(key === '..') {
+		if(prompt.context_stack.length <= 1) {
+			prompt.emit('error', 'Cannot change to parent context: Already at the root context.');
+			return;
+		}
+		var context = prompt.context_stack.pop();
+		prompt.context = context;
+		return;
+	}
+	
+	if(!context.hasOwnProperty(key)) {
+		prompt.emit('error', 'No such keyword: ' + key);
+		return;
+	}
+	if(typeof context[key] != 'object') {
+		prompt.emit('error', 'Illegal context: ' + key);
+		return;
+	}
+	var new_context = context[key];
+	prompt.context_stack.push(new_context);
+	prompt.context = new_context;
+}
+
+var buildin_context = {
+	'ls': ls,
+	'cd': cd,
+	'exit': exit,
+	'quit': exit,
+	'set':set_prop
+}
+
 var context = {
-	'help': help,
+	'users': [
+		{'name':'foo'},
+		{'name':'bar'}
+	],
 	'echo': function(a, b, c) {
 		console.log(a + ", " + b + ", " + c);
-	},
-	'exit': exit,
-	'quit': exit
+	}
 };
 
-var prompt = shell.start(context);
+var prompt = shell.start(context, buildin_context);
 
 prompt.on('error', function(msg) {
 	console.error(msg);
--- protos/simple-shell.js
+++ protos/simple-shell.js
@@ -2,15 +2,20 @@
 
 var trim = require('snippets').trim;
 
-function simple_shell(context, prefix) {
+function simple_shell(context, buildin_context, prefix) {
 	var prefix = prefix || '> ',
 	    prompt = require('./prompt.js').create(prefix);
+	prompt.context_stack = [context];
+	prompt.context = context;
+	prompt.buildin_context = buildin_context;
 	prompt.on('line', function(line) {
 		var args = trim(line).split(/ +/),
 		    cmd = args.shift();
 		
-		if(context[cmd] && (typeof context[cmd] === 'function')) {
-			context[cmd].apply(context, args);
+		if(prompt.buildin_context[cmd] && (typeof prompt.buildin_context[cmd] === 'function')) {
+			prompt.buildin_context[cmd].apply(prompt, args);
+		} else if(prompt.context[cmd] && (typeof prompt.context[cmd] === 'function')) {
+			prompt.context[cmd].apply(prompt, args);
 		} else {
 			prompt.emit('error', 'Unknown command: ' + cmd);
 		}
